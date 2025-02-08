54e1f1a36c736c718387811a550d82b800f11913
Nick A <nicolas.a.asanov@gmail.com>
2025-02-08 18:19:37 -0500

    wip

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,9 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 export default function TopBar() {
   return (
-    <View>
-      <Text>My App</Text>
+    <View style={styles.container}>
+      <View style={{ width: 25, height: 25, borderRadius: 25, backgroundColor: 'black' }} />
+      <View style={{ width: 25, height: 25, borderRadius: 25, backgroundColor: 'red' }} />
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 16,
+  }
+})
