9bf8375c796f230c2f644caaf55ecd64c6a4ec77
VidurShah <vidurshah12@gmail.com>
2025-02-09 10:37:12 -0500

    finished merging chatbotapi branch with main

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import { Platform } from "react-native";
 import { HapticTab } from "@/components/HapticTab";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
-import { Home, History, User } from "lucide-react";
+import { Home, History, User, Bot } from "lucide-react";
 import TopBar from "@/components/TopBar";
 
 export default function TabLayout() {
@@ -46,7 +46,15 @@ export default function TabLayout() {
             tabBarIcon: ({ color }) => <User size={24} color={color} />,
           }}
         />
+        <Tabs.Screen
+          name="chat"
+          options={{
+            title: "Pedro",
+            tabBarIcon: ({ color }) => <Bot size={24} color={color} />,
+          }}
+        />
       </Tabs>
     </>
   );
 }
+
